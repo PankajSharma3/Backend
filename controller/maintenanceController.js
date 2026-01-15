e6b6b263d52090c92d9ec7c7d0b693629f5293d6
Pankaj <ps90798173@gmail.com>
2026-01-15 19:21:36 +0530

    Update in the model controller

--- controller/maintenanceController.js
+++ controller/maintenanceController.js
@@ -27,7 +27,7 @@ export const createMaintenance = async (req, res) => {
 
         const newMaintenance = new Maintenance({
             title,
-            description,
+            note: description,
             priority: priority || 'medium',
             displayName,
             submittedBy,
